7cfcd38369f64b75cfe3b5008b07697b4c84c502
stoneskill <244540499@qq.com>
2017-06-01 10:10:50 +0800

    add x_process_get

---
+++ src/x_process_get/x_process_get.c
@@ -0,0 +1,52 @@
+﻿#include <windows.h>  
+#include <tlhelp32.h> //进程快照函数头文件  
+#include <stdio.h>  
+
+int main()
+{
+	int i = 0; 
+	int countProcess = 0;                                 //当前进程数量计数变量  
+	PROCESSENTRY32 currentProcess;                      //存放快照进程信息的一个结构体  
+	currentProcess.dwSize = sizeof(currentProcess);     //在使用这个结构之前，先设置它的大小  
+	HANDLE hProcess = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);//给系统内的所有进程拍一个快照  
+	char big_chars[1024] = { 0 };
+
+
+	if (hProcess == INVALID_HANDLE_VALUE)
+	{
+		printf("CreateToolhelp32Snapshot()调用失败!\n");
+		return -1;
+	}
+
+	BOOL bMore = Process32First(hProcess, &currentProcess);  //获取第一个进程信息  
+	while (bMore)
+	{
+		memset(big_chars, 0, sizeof(big_chars));
+
+		for (i = 0; i < strlen(currentProcess.szExeFile); i++)
+		{
+			if (currentProcess.szExeFile[i]>= 'a' && currentProcess.szExeFile[i]<= 'z')
+			{
+				big_chars[i] = currentProcess.szExeFile[i] - 32;
+			}
+			else
+			{
+				big_chars[i] = currentProcess.szExeFile[i];
+			}
+
+			if (memcmp(big_chars, "ADSAFE", 6) == 0)
+			{
+				printf("PID=%5u    PName= %s\n", currentProcess.th32ProcessID, currentProcess.szExeFile); //遍历进程快照，轮流显示每个进程信息  
+			}
+		}
+
+
+		bMore = Process32Next(hProcess, &currentProcess);    //遍历下一个  
+		countProcess++;
+	}
+
+	CloseHandle(hProcess);  //清除hProcess句柄  
+	printf("共有以上%d个进程在运行\n", countProcess);
+	system("pause");
+	return 0;
+}
