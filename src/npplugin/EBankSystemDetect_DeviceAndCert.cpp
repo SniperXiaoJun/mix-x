183d8ee3db2d8878113ee03ea2cac06da248a6e7
stoneskill <244540499@qq.com>
2017-06-14 15:06:43 +0800

    fix not used files

--- src/npplugin/EBankSystemDetect_DeviceAndCert.cpp
+++ src/npplugin/EBankSystemDetect_DeviceAndCert.cpp
@@ -28,8 +28,6 @@ std::string g_CurrentCerts;
 
 int CollectUSBInfo(int * piCount, char * pVID, char * pPID)
 {  
-
-#if defined(MIX_SHUNDE_BANK)
 	int i = -1;
 	int count = 0;
 	// 获取当前系统所有使用的设备  
@@ -37,95 +35,7 @@ int CollectUSBInfo(int * piCount, char * pVID, char * pPID)
 	HDEVINFO hDevInfo = SetupDiGetClassDevs(NULL, NULL, NULL, dwFlag);  
 	if( INVALID_HANDLE_VALUE == hDevInfo )  
 	{   
-		return -1;  
-	}  
-
-	// 准备遍历所有设备查找USB  
-	SP_DEVINFO_DATA sDevInfoData;  
-	sDevInfoData.cbSize = sizeof(SP_DEVINFO_DATA);   
-	std::string strText;  
-	TCHAR szDIS[MAX_PATH]; // Device Identification Strings,   
-	DWORD nSize = 0 ;  
-	for(int i = 0; SetupDiEnumDeviceInfo(hDevInfo,i,&sDevInfoData); i++ )  
-	{  
-		nSize = 0;  
-		if ( !SetupDiGetDeviceInstanceId(hDevInfo, &sDevInfoData, szDIS, sizeof(szDIS), &nSize) )  
-		{  
-			goto err;
-		}  
-
-		// 设备识别串的前三个字符是否是"USB", 模板： USB\VID_XXXX&PID_XXXX\00000xxxxxxx  
-		std::string strDIS( szDIS );  
-
-		transform(strDIS.begin(),strDIS.end(),strDIS.begin(),toupper);
-
-		if(strDIS.substr( 0,3 ) == std::string("USB") )  
-		{ 
-			count++;
-		}  
-	}  
-
-	*piCount = count;
-
-err:
-
-	// 释放设备  
-	SetupDiDestroyDeviceInfoList(hDevInfo);  
-
-	return 0;
-
-#elif defined(MIX_JILIN_BANK)
-	int i = -1;
-	int count = 0;
-	// 获取当前系统所有使用的设备  
-	DWORD dwFlag = (DIGCF_ALLCLASSES | DIGCF_PRESENT);  
-	HDEVINFO hDevInfo = SetupDiGetClassDevs(NULL, NULL, NULL, dwFlag);  
-	if( INVALID_HANDLE_VALUE == hDevInfo )  
-	{   
-		return -1;  
-	}  
-
-	// 准备遍历所有设备查找USB  
-	SP_DEVINFO_DATA sDevInfoData;  
-	sDevInfoData.cbSize = sizeof(SP_DEVINFO_DATA);   
-	std::string strText;  
-	TCHAR szDIS[MAX_PATH]; // Device Identification Strings,   
-	DWORD nSize = 0 ;  
-	for(int i = 0; SetupDiEnumDeviceInfo(hDevInfo,i,&sDevInfoData); i++ )  
-	{  
-		nSize = 0;  
-		if ( !SetupDiGetDeviceInstanceId(hDevInfo, &sDevInfoData, szDIS, sizeof(szDIS), &nSize) )  
-		{  
-			goto err;
-		}  
-
-		// 设备识别串的前三个字符是否是"USB", 模板： USB\VID_XXXX&PID_XXXX\00000xxxxxxx  
-		std::string strDIS( szDIS );  
-
-		transform(strDIS.begin(),strDIS.end(),strDIS.begin(),toupper);
-
-		if(strDIS.substr( 0,3 ) == std::string("USB") )  
-		{ 
-			count++;
-		}  
-	}  
-
-	*piCount = count;
-
-err:
-
-	// 释放设备  
-	SetupDiDestroyDeviceInfoList(hDevInfo);  
-
-	return 0;
-#else
-	int i = -1;
-	int count = 0;
-	// 获取当前系统所有使用的设备  
-	DWORD dwFlag = (DIGCF_ALLCLASSES | DIGCF_PRESENT);  
-	HDEVINFO hDevInfo = SetupDiGetClassDevs(NULL, NULL, NULL, dwFlag);  
-	if( INVALID_HANDLE_VALUE == hDevInfo )  
-	{   
+		*piCount = 0;
 		return -1;  
 	}  
 
@@ -150,52 +60,55 @@ err:
 
 		if(strDIS.substr( 0,3 ) == std::string("USB") )  
 		{  
-			strText += strDIS;  
-			strText += "\r\n";  
-			int iVID_Pos = strstr(strDIS.c_str(),pVID) - strDIS.c_str();  
-			if( iVID_Pos == 8 )  
-			{  
-				// VID: 厂商号  
-				//printf( NIKON_ID  );  
-				//printf( "\n");  
-				// PID :产品号  
-				int iSlashPos = 0;
-
-				for (const char * p = strDIS.c_str() + strDIS.size();p > strDIS.c_str(); p--)
+			if (NULL == pVID || NULL == pPID)
+			{
+				count++;
+			}
+			else
+			{
+				strText += strDIS;
+				strText += "\r\n";
+				int iVID_Pos = strstr(strDIS.c_str(), pVID) - strDIS.c_str();
+				if (iVID_Pos == 8)
 				{
-					if (*p == '\\')
+					// VID: 厂商号  
+					//printf( NIKON_ID  );  
+					//printf( "\n");  
+					// PID :产品号  
+					int iSlashPos = 0;
+
+					for (const char * p = strDIS.c_str() + strDIS.size(); p > strDIS.c_str(); p--)
 					{
-						iSlashPos = p - strDIS.c_str();
-						break;
+						if (*p == '\\')
+						{
+							iSlashPos = p - strDIS.c_str();
+							break;
+						}
 					}
+
+					int iPID_Pos = strstr(strDIS.c_str(), "PID_") - strDIS.c_str();
+					std::string strProductID = strDIS.substr(iPID_Pos + 4, iSlashPos - iPID_Pos - 4);
+
+					if (std::string(pPID) == strProductID)
+					{
+						count++;
+					}
+
+					// 序列号  
+					int iRight = strDIS.size() - iSlashPos - 1;
+					std::string strSerialNumber = strDIS.substr(iSlashPos + 1, iRight);
 				}
-
-				int iPID_Pos = strstr(strDIS.c_str(),"PID_") - strDIS.c_str();
-				std::string strProductID = strDIS.substr( iPID_Pos+4, iSlashPos - iPID_Pos - 4 );  
-
-				if(std::string(pPID) == strProductID)
-				{
-					count++;
-				}
-
-				// 序列号  
-				int iRight = strDIS.size() - iSlashPos  -1;  
-				std::string strSerialNumber = strDIS.substr(iSlashPos+1 ,iRight );  
-
-			}  
+			}
 		}  
 	}  
 
 	*piCount = count;
-
 err:
 
 	// 释放设备  
 	SetupDiDestroyDeviceInfoList(hDevInfo);  
 
 	return 0;
-#endif
-	
 }
 
 //95568非国密：
