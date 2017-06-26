5aca804a3c6a4ef72d6a07b7cdd3e912d7f0f601
stoneskill <244540499@qq.com>
2017-06-26 13:46:46 +0800

    ADD TEST

---
+++ src/dbcs/x_dbcs_enum_fileinfo.cpp
@@ -0,0 +1,22 @@
+﻿
+#include<iostream>
+#include <fstream>
+#include<string>
+#include<io.h>
+#include <Windows.h>
+#include <wincrypt.h>
+#include <stdio.h>
+#include <smb_qtui.h>
+#include <smb_cs.h>
+
+int main(int argc, char * argv[])
+{
+	SMB_CS_FileInfo_NODE * pHeader = NULL;
+
+	SMB_CS_SetPath("smb_cs.db");
+	SMB_CS_Init();
+
+	SMB_CS_EnumFileInfo(&pHeader);
+
+	return 0;
+}
