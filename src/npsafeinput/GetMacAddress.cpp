dfedbf6452dec8b22e19c380eb85dddf29e8de15
stoneskill <244540499@qq.com>
2017-07-06 17:46:27 +0800

    add npsafeinput plugins

---
+++ src/npsafeinput/GetMacAddress.cpp
@@ -0,0 +1,78 @@
+#include "GetMacAddress.h"
+#include <iostream>using namespace std;
+
+GetMacAddress::GetMacAddress()
+{
+}
+
+
+GetMacAddress::~GetMacAddress()
+{
+}
+
+const char * GetMacAddress::GetMac(){
+	std::string str;
+    //PIP_ADAPTER_INFO结构体指针存储本机网卡信息
+    PIP_ADAPTER_INFO pIpAdapterInfo = new IP_ADAPTER_INFO();
+    //得到结构体大小,用于GetAdaptersInfo参数
+    unsigned long stSize = sizeof(IP_ADAPTER_INFO);
+    //调用GetAdaptersInfo函数,填充pIpAdapterInfo指针变量;其中stSize参数既是一个输入量也是一个输出量
+    int nRel = GetAdaptersInfo(pIpAdapterInfo,&stSize);
+    if (ERROR_BUFFER_OVERFLOW==nRel)
+    {
+		//如果函数返回的是ERROR_BUFFER_OVERFLOW
+		//则说明GetAdaptersInfo参数传递的内存空间不够,同时其传出stSize,表示需要的空间大小
+		//这也是说明为什么stSize既是一个输入量也是一个输出量
+		//释放原来的内存空间
+        delete pIpAdapterInfo;
+        //重新申请内存空间用来存储所有网卡信息
+        pIpAdapterInfo = (PIP_ADAPTER_INFO)new BYTE[stSize];
+        //再次调用GetAdaptersInfo函数,填充pIpAdapterInfo指针变量
+        nRel=GetAdaptersInfo(pIpAdapterInfo,&stSize);    
+    }
+    if (ERROR_SUCCESS==nRel)
+    {
+        //输出网卡信息
+		//可能有多网卡,因此通过循环去判断
+		while (pIpAdapterInfo)
+		{
+			//cout<<"网卡名称："<<pIpAdapterInfo->AdapterName<<endl;
+			//cout<<"网卡描述："<<pIpAdapterInfo->Description<<endl;
+			//cout<<"网卡MAC地址："<<pIpAdapterInfo->Address;
+			for (UINT i = 0; i < pIpAdapterInfo->AddressLength; i++)
+			if (i==pIpAdapterInfo->AddressLength-1)
+			{
+				//strcpy(m_Mac, (char *)pIpAdapterInfo->Address[i]);
+				MessageBoxA(NULL,(char *)pIpAdapterInfo->Address[i],"",0);
+				//str.append((char *)pIpAdapterInfo->Address[i]).append("|");
+				//str+=(char *)pIpAdapterInfo->Address[i]+"|";
+				//m_Mac+=(char *)pIpAdapterInfo->Address[i]+"\n";
+				printf("%02x\n", pIpAdapterInfo->Address[i]);
+			}
+			else
+			{
+				MessageBoxA(NULL,(char *)pIpAdapterInfo->Address[i],"",0);
+//				str.append((char *)pIpAdapterInfo->Address[i]).append("-");
+				//str+=(char *)pIpAdapterInfo->Address[i]+"-";
+				printf("%02x-", pIpAdapterInfo->Address[i]);
+			}
+			//cout<<"网卡IP地址如下："<<endl;
+			//可能网卡有多IP,因此通过循环去判断
+			//IP_ADDR_STRING *pIpAddrString =&(pIpAdapterInfo->IpAddressList);
+			//do 
+			//{
+			//	cout<<pIpAddrString->IpAddress.String<<endl;
+			//	pIpAddrString=pIpAddrString->Next;
+			//} while (pIpAddrString);
+			//pIpAdapterInfo = pIpAdapterInfo->Next;
+			//cout<<"*****************************************************"<<endl;
+		}
+    }
+    //释放内存空间
+    if (pIpAdapterInfo)
+    {
+        delete pIpAdapterInfo;
+    }
+	MessageBoxA(NULL,str.c_str(),"",0);
+	return str.c_str();
+}
---
+++ src/npsafeinput/PluginObject.cpp
@@ -0,0 +1,165 @@
+#include "PluginObject.h"
+#include "plugin.h"
+
+PluginObject::PluginObject(NPP npp){	this->npp = npp;	this->id_property_version = NPN_GetStringIdentifier("version");}
+PluginObject::~PluginObject(void)
+{
+}
+void PluginObject::deallocate()
+{
+}
+
+void PluginObject::invalidate()
+{
+}
+
+bool PluginObject::hasMethod(NPIdentifier methodName)
+{
+	return false;
+}
+
+bool PluginObject::invokeDefault(const NPVariant *args, uint32_t argCount, NPVariant *result)
+{
+	return true;
+}
+
+bool PluginObject::invoke(NPIdentifier methodName, const NPVariant *args, uint32_t argCount, NPVariant *result)
+{
+	if(false){
+	
+	}
+	else{
+		NPN_InvokeDefault(npp,this,args,argCount,result);	
+	}
+	return true;
+}
+
+bool PluginObject::hasProperty(NPIdentifier propertyName)
+{
+	if(propertyName == this->id_property_version)
+		return true;
+	return false;
+}
+
+bool PluginObject::getProperty(NPIdentifier propertyName, NPVariant *result)
+{
+	if(propertyName == this->id_property_version)
+	{
+		CPlugin * pPlugin=(CPlugin*)npp->pdata;
+		if(pPlugin)
+		{
+			//pPlugin->Dlg->GetVal();
+			VOID_TO_NPVARIANT(*result);
+			char *outString=pPlugin->GetValue();
+
+			char *npOutString=(char *)NPN_MemAlloc(strlen(outString)+1);
+
+			if(!npOutString)
+				return false;
+			strcpy(npOutString,outString);
+
+			STRINGZ_TO_NPVARIANT(npOutString,*result);
+		}
+
+		//VOID_TO_NPVARIANT(*result);
+		//char *outString=pPlugin->Dlg->GetVal();
+		//char *npOutString=(char *)NPN_MemAlloc(strlen(outString)+1);
+
+		//if(!npOutString)
+		//	return false;
+		//strcpy(npOutString,outString);
+
+		//STRINGZ_TO_NPVARIANT(npOutString,*result);
+
+		//int plugin_major, plugin_minor, netscape_major,netscape_minor;
+		//NPN_Version(&plugin_major,&plugin_minor,&netscape_major,&netscape_minor);
+		//NPString version = "";
+		//char plugin_major_str[10];
+		//itoa(plugin_major,plugin_major_str,10);
+
+		//char plugin_minor_str[10];
+		//itoa(plugin_minor,plugin_minor_str,10);
+
+		//char netscape_major_str[10];
+		//itoa(netscape_major,netscape_major_str,10);
+
+		//char netscape_minor_str[10];
+		//itoa(netscape_minor,netscape_minor_str,10);
+		//version.append("plugin_major:").append(plugin_major_str);
+		//version.append("\nplugin_minor:").append(plugin_minor_str);
+		//version.append("\nnetscape_major:").append(netscape_major_str);
+		//version.append("\nnetscape_minor:").append(netscape_minor_str);
+		//MessageBoxA(NULL,version.c_str(),"",NULL);
+		return true;
+	}
+	return false;
+}
+bool PluginObject::setProperty(NPIdentifier name,const NPVariant *value)
+{
+	return true;
+}
+bool PluginObject::removeProperty(NPIdentifier name)
+{
+	return true;
+}
+//这玩意折腾不少时间了!
+bool PluginObject::enumerate(NPIdentifier **identifier,uint32_t *count){	return false;}
+bool PluginObject::construct(const NPVariant *args,uint32_t argCount, NPVariant *result){	return true;}
+NPObject* PluginObject::_allocate(NPP npp,NPClass* aClass){	return new PluginObject(npp);}
+
+void PluginObject::_deallocate(NPObject *npobj)
+{
+	((PluginObject*)npobj)->deallocate();
+	if(npobj)
+		delete npobj;
+}
+
+void PluginObject::_invalidate(NPObject *npobj)
+{
+	((PluginObject*)npobj)->invalidate();
+}
+
+bool PluginObject::_hasMethod(NPObject* obj, NPIdentifier methodName)
+{
+	return ((PluginObject*)obj)->hasMethod(methodName);
+}
+
+bool PluginObject::_invokeDefault(NPObject *obj, const NPVariant *args, uint32_t argCount, NPVariant *result)
+{
+	return ((PluginObject*)obj)->invokeDefault(args,argCount,result);
+}
+
+bool PluginObject::_invoke(NPObject* obj, NPIdentifier methodName, const NPVariant *args, uint32_t argCount, NPVariant *result)
+{
+	return ((PluginObject*)obj)->invoke(methodName,args,argCount,result);
+}
+
+bool PluginObject::_hasProperty(NPObject *obj, NPIdentifier propertyName)
+{
+	return ((PluginObject*)obj)->hasProperty(propertyName);
+}
+
+bool PluginObject::_getProperty(NPObject *obj, NPIdentifier propertyName, NPVariant *result)
+{
+	return ((PluginObject*)obj)->getProperty(propertyName,result);
+}
+
+bool PluginObject::_setProperty(NPObject *npobj, NPIdentifier name,const NPVariant *value)
+{
+	return ((PluginObject*)npobj)->setProperty(name,value);
+}
+
+bool PluginObject::_removeProperty(NPObject *npobj, NPIdentifier name)
+{
+	return ((PluginObject*)npobj)->removeProperty(name);
+}
+
+bool PluginObject::_enumerate(NPObject *npobj, NPIdentifier **identifier,uint32_t *count)
+{
+	return ((PluginObject*)npobj)->enumerate(identifier,count);
+}
+
+bool PluginObject::_construct(NPObject *npobj, const NPVariant *args,uint32_t argCount, NPVariant *result)
+{
+	return ((PluginObject*)npobj)->construct(args,argCount,result);
+}
