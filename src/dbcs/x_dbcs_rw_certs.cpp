2fe829bf23bdcc1e5dea75186b4bd0fa42d83018
stoneskill <244540499@qq.com>
2017-07-25 09:42:09 +0800

    add rw certs

---
+++ src/dbcs/x_dbcs_rw_certs.cpp
@@ -0,0 +1,59 @@
+
+#include <smb_cs.h>
+#include <smb_qtui.h>
+#include <smb_dev.h>
+#include "json/json.h"
+#include<iostream>
+#include <fstream>
+#include<string>
+#include<io.h>
+
+using namespace std;
+
+unsigned int ADD_USER_CERTS(SMB_CS_CertificateContext_NODE *pCertCtxNode)
+{
+	SMB_CS_ClrAllCertCtx(2);
+
+	while (pCertCtxNode)
+	{
+		if (SMB_CERT_ALG_FLAG_RSA == pCertCtxNode->ptr_data->stAttr.ucCertAlgType)
+		{
+			
+		}
+		else
+		{
+			SMB_CS_AddCertCtx(pCertCtxNode->ptr_data, 2);
+		}
+		pCertCtxNode = pCertCtxNode->ptr_next;
+	}
+
+	return 0;
+}
+
+int main(int argc, char * argv[])
+{
+	SMB_CS_CertificateContext_NODE *header = NULL;
+
+	SMB_CS_CertificateContext *ctx = NULL;
+
+	char smb_db_path[1024] = { 0 };
+
+	GetEnvironmentVariableA("APPDATA", smb_db_path, MAX_PATH);
+
+	strcat(smb_db_path, "\\suloong_sm.smb_cs.db");
+
+	SMB_CS_SetPath("smb_cs.db");
+
+	SMB_CS_Init();
+
+	SMB_DEV_EnumCert(&header, SMB_CERT_ALG_FLAG_SM2,
+		SMB_CERT_USAGE_FLAG_SIGN | SMB_CERT_USAGE_FLAG_EX, 
+		SMB_CERT_VERIFY_FLAG_TIME | SMB_CERT_VERIFY_FLAG_CHAIN | SMB_CERT_VERIFY_FLAG_CRL,
+		SMB_CERT_FILTER_FLAG_FALSE);
+
+	ADD_USER_CERTS(header);
+
+	SMB_CS_FreeCertCtxLink(&header);
+
+	return 0;
+}
