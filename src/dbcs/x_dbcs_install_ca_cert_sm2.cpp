7ba55dddeaa7bd18b4ef9c15dfa8300870992b48
stoneskill <244540499@qq.com>
2017-07-31 13:47:44 +0800

    fix sm2

--- src/dbcs/x_dbcs_install_ca_cert_sm2.cpp
+++ src/dbcs/x_dbcs_install_ca_cert_sm2.cpp
@@ -101,7 +101,7 @@ void filesearch(string path, int layer)
 				_file.read((char *)pbCaCert, length>sizeof(pbCaCert) ? sizeof(pbCaCert) : length);
 				_file.close();
 
-				SMB_CS_ImportCaCert(pbCaCert, length>sizeof(pbCaCert) ? sizeof(pbCaCert) : length, &ulAlgType);
+				SMB_CS_ImportCaCertSM2(pbCaCert, length>sizeof(pbCaCert) ? sizeof(pbCaCert) : length, &ulAlgType);
 			}
 			else
 			{
